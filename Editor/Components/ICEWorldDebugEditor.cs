e851d3e1dd39cbb16728076599b3d8357fdc59cf
icetec <pit@ice-technologies.de>
2016-06-02 09:44:19 +0200

    Insert ICEWorldDebugEditor

---
+++ Editor/Components/ICEWorldDebugEditor.cs
@@ -0,0 +1,77 @@
+﻿// ##############################################################################
+//
+// ICEWorldDebugEditor.cs
+// Version 1.2.10
+//
+// © Pit Vetterick, ICE Technologies Consulting LTD. All Rights Reserved.
+// http://www.icecreaturecontrol.com
+// mailto:support@icecreaturecontrol.com
+// 
+// Unity Asset Store End User License Agreement (EULA)
+// http://unity3d.com/legal/as_terms
+//
+// ##############################################################################
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+
+using ICE;
+using ICE.World;
+using ICE.World.EditorUtilities;
+using ICE.World.Utilities;
+
+namespace ICE.World
+{
+	[CustomEditor(typeof(ICEWorldBehaviour))]
+	public class ICEWorldDebugEditor : Editor 
+	{
+		protected EditorHeaderType m_HeaderType = EditorHeaderType.FOLDOUT_ENABLED_BOLD;
+
+		/// <summary>
+		/// Raises the inspector GUI event.
+		/// </summary>
+		public override void OnInspectorGUI()
+		{
+			ICEWorldBehaviour _target = DrawDefaultHeader<ICEWorldBehaviour>();
+
+			DrawDefaultFooter( _target );
+
+		}
+
+		/// <summary>
+		/// Draws the default header.
+		/// </summary>
+		/// <returns>The default header.</returns>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public virtual T DrawDefaultHeader<T>() where T : ICEWorldBehaviour
+		{
+			ICEEditorLayout.Init();
+			T _target = (T)target;
+
+			GUI.changed = false;
+			//Info.HelpButtonIndex = 0;
+
+			EditorGUILayout.Separator();
+			return _target;
+		}
+
+		/// <summary>
+		/// Draws the default footer.
+		/// </summary>
+		/// <param name="_target">Target.</param>
+		public virtual void DrawDefaultFooter( ICEWorldBehaviour _target )
+		{
+			if( _target == null )
+				return;
+
+			EditorGUILayout.Separator();
+			if( GUI.changed )
+				EditorUtility.SetDirty( _target );
+		}
+	}
+}
+
